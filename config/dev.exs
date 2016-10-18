9775cad66916debd1976e9621677a21c7e27bce9
azhi <astzhe@gmail.com>
2016-10-19 00:17:20 +0300

    removed leftover of old config from dev.exs
    that part is now located in config/dev.secret.exs.example

--- config/dev.exs
+++ config/dev.exs
@@ -11,9 +11,4 @@ config :logger, :console, format: "[$level] $message\n"
 
 config :phoenix, :stacktrace_depth, 20
 
-config :poscaster, Poscaster.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL"},
-  pool_size: 10
-
 import_config "dev.secret.exs"
